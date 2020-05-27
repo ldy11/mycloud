7cbe63aa02ff7039c74bf0a24c8cf925c291efe8
chenhan <1220298224@qq.com>
2020-05-27 14:08:26 +0800

    初版

---
+++ common/src/main/java/com/ldy/cloud/mycloud/model/User.java
@@ -0,0 +1,10 @@
+package com.ldy.cloud.mycloud.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String height;
+    private String name;
+    private int age;
+}
---
+++ src/test/java/com/ldy/cloud/mycloud/MycloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldy.cloud.mycloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
